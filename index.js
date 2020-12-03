48bc00bf0b513224ef9d7d60923b7fb3b7435fca
zzz664 <zzz664>
2020-12-04 00:29:10 +0900

    2020-12-04 login기능 어렵다.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/keys');
 
 const {User} = require('./models/User');
@@ -10,6 +11,7 @@ const {User} = require('./models/User');
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json 형태를 가져올 수 있게함
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -27,14 +29,43 @@ app.get('/', (req, res) => res.send('Hello World!'));
     password: "password"
 }
 */
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
+
     user.save((err, userInfo) => {
         if(err) 
             return res.json({success: false, err})
 
         return res.status(200).json({success: true})
     })
+});
+
+app.post('/api/users/login', (req, res) => {
+    //요청된 email을 db에서 찾기
+    User.findOne({email: req.body.email}, (err, userInfo) => {
+        if(!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "존재하지 않는 이메일입니다."
+            });
+        }
+        //요청된 email이 유효하면 비밀번호가 맞는지 확인
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 틀렸습니다."
+            });
+            //유저토큰 생성
+            userInfo.genToken((err, userInfo) => {
+                if(err) return res.status(400).send(err); //400은 오류가 있다는 뜻
+                //토큰을 저장한다. (쿠키, 로컬스토리지, 세션 등등..)
+                res.cookie("x_auth", userInfo.token).status(200).json({
+                    loginSuccess: true,
+                    userId: userInfo._id
+                });
+            });
+        })
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
