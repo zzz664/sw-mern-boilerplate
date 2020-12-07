e10e180cf62262669083799a3896fc0dd0954df9
zzz664 <zzz664>
2020-12-07 18:32:41 +0900

    2020-12-07 redux, middleware, extention 셋팅

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import 'antd/dist/antd.css';
+
+import {Provider} from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiserMiddleware, reduxThunk)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(Reducer,
+       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+       window.__REDUX_DEVTOOLS_EXTENSION__()
+       )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
